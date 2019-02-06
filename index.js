78697714fe0093e1b470a91c43dd0639bf762081
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-06 15:58:48 -0800

    added user type

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const { ApolloServer } = require('apollo-server');
 
-var photos = [];
-var _id = 0;
+let users = [
+  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
+  { githubLogin: 'gPlake', name: 'Glen Plake' },
+  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' }
+];
+
+let photos = [
+  {
+    id: '1',
+    name: 'Dropping the Heart Chute',
+    description: 'The heart chute is one of my favorite chutes',
+    category: 'ACTION',
+    githubUser: 'gPlake'
+  },
+  {
+    id: '2',
+    name: 'Enjoying the sunshine',
+    category: 'SELFIE',
+    githubUser: 'sSchmidt'
+  },
+  {
+    id: '3',
+    name: 'Gunbarrel 25',
+    description: '25 laps on gunbarrel today',
+    category: 'LANDSCAPE',
+    githubUser: 'sSchmidt'
+  }
+];
+
+let _id = 0;
 
 const typeDefs = `
 enum PhotoCategory {
@@ -12,12 +40,20 @@ enum PhotoCategory {
   GRAPHIC
 }
 
+type User {
+  githubLogin: ID!
+  name: String
+  avatar: String
+  postedPhotos: [Photo!]!
+}
+
 type Photo {
   id: ID!
   url: String!
   name: String!
   description: String
   category: PhotoCategory!
+  postedBy: User!
 }
 
 type Query {
@@ -58,7 +94,16 @@ const resolvers = {
   },
 
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(user => user.githubLogin === parent.githubUser);
+    }
+  },
+
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(photo => photo.githubUser === parent.githubLogin);
+    }
   }
 };
 
