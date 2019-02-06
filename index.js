f60cb7dc9fde243886e65898e969eadbb6852da5
Hasier Pastor <hasier.pastor@gmail.com>
2019-02-06 15:33:30 -0800

    added post photo query and resolver

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const { ApolloServer } = require('apollo-server');
 
+var photos = [];
+var _id = 0;
+
 const typeDefs = `
+type Photo {
+  id: ID!
+  url: String!
+  name: String!
+  description: String
+}
 type Query {
   totalPhotos: Int!
-}`;
+  allPhotos: [Photo!]!
+}
+type Mutation {
+  postPhoto(name: String! description: String): Photo!
+}
+`;
 
 const resolvers = {
+  //resolver for total photos => return length of photo array
   Query: {
-    totalPhotos: () => 42
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
+  },
+  //mutation resolver for posting a photo => returns true
+  //parent parameter refers to parent object - int this case a Mutation.
+  //Always the first argument for a mutation.
+  Mutation: {
+    postPhoto(parent, args) {
+      var newPhoto = {
+        id: _id++,
+        ...args
+      };
+      photos.push(newPhoto);
+
+      return newPhoto;
+    }
   }
 };
 
